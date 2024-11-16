ba1afa2a2d85a6c6d3a53f45e0d287268e8445ce
BarfaidYassine <ybarfaid@gmail.com>
2024-11-16 14:29:43 +0100

    add icon and title

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <header>
+    <title>Portfolio</title>
+  </header>
     <App />
   </BrowserRouter>
 );
